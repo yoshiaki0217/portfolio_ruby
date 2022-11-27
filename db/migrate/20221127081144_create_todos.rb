6f68d8ea7238165b819504be99d553603adf1279
大城由晃 <yoshiaki19980217@icloud.com>
2022-11-27 17:21:34 +0900

    maigrete/seeder実行

--- db/migrate/20221127081144_create_todos.rb
+++ db/migrate/20221127081144_create_todos.rb
@@ -1,8 +1,10 @@
-def change
-  create_table :todos do |t|
-    t.string :name, null: false
-    t.boolean :is_completed, default: false, null: false
+class CreateTodos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :todos do |t|
+      t.string :name, null: false
+      t.boolean :is_completed, default: false, null: false
 
-    t.timestamps
+      t.timestamps
+    end
   end
 end
